420970a4eb287b060931cc99b32f8931e03cb8f2
Lixo <luis.rojas04@alumnos.ucn.cl>
2020-11-06 12:59:24 -0300

    desacoplamiento tooltip

--- gui.py
+++ gui.py
@@ -4,34 +4,8 @@ Implementación de distribuciones.py para escritorio con interfaz grafica
 from tkinter import Entry, Label, Button, Tk, StringVar, OptionMenu
 
 from distribuciones import distribuciones
-from utils import key_to_list_from_dict
+from utils import key_to_list_from_dict, CreateToolTip
 
-class CreateToolTip(object):
-    '''
-    Create a tooltip for a given widget
-    '''
-    def __init__(self, widget, text='widget info'):
-        self.widget = widget
-        self.text = text
-        self.widget.bind("<Enter>", self.enter)
-        self.widget.bind("<Leave>", self.close)
-    def enter(self, event=None):
-        x = y = 0
-        x, y, cx, cy = self.widget.bbox("insert")
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
-        # creates a toplevel window
-        self.tw = Toplevel(self.widget)
-        # Leaves only the label and removes the app window
-        self.tw.wm_overrideredirect(True)
-        self.tw.wm_geometry("+%d+%d" % (x, y))
-        label = Label(self.tw, text=self.text, justify='left',
-                       background='light green', relief='solid', borderwidth=1,
-                       font=("sans", "12", "bold"))
-        label.pack(ipadx=1)
-    def close(self, event=None):
-        if self.tw:
-            self.tw.destroy()
 
 
 def flush_widgets(frame):
--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
 """
 Colección de metodos de utilidad para las implementaciones
 """
+from tkinter import *
+class CreateToolTip(object):
+    '''
+    Create a tooltip for a given widget
+    '''
+    def __init__(self, widget, text='widget info'):
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.close)
+    def enter(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 20
+        # creates a toplevel window
+        self.tw = Toplevel(self.widget)
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = Label(self.tw, text=self.text, justify='left',
+                       background='light green', relief='solid', borderwidth=1,
+                       font=("sans", "12", "bold"))
+        label.pack(ipadx=1)
+    def close(self, event=None):
+        if self.tw:
+            self.tw.destroy()
 
 
 def key_to_list_from_dict(dictionary):
@@ -8,3 +35,4 @@ def key_to_list_from_dict(dictionary):
     Obtiene una lista compuesta por las claves del diccionario recibido
     """
     return [key for key in dictionary.keys()]
+
